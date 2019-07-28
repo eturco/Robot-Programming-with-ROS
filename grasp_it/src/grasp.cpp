67977c43bb387396244d713af89f62a5a17434d5
Enrico Turco <44170339+eturco@users.noreply.github.com>
2019-07-28 21:09:39 +0200

    Add files via upload

--- grasp_it/src/grasp.cpp
+++ grasp_it/src/grasp.cpp
@@ -24,28 +24,30 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <sensor_msgs/JointState.h>
 
+#include <std_srvs/SetBool.h>
 
+ros::Publisher pub;
+std_msgs::Float64 state;
+
+//bool ack(std_srvs::SetBool::Request  &req,
+//         std_srvs::SetBool::Response &res)
+// {
+//   res.success = 1;
+//   ROS_INFO("Grasp!");
+//   return true;
+// };
+
+void jointStates(const sensor_msgs::JointState msg) {
+    ros::Time now = ros::Time::now();
+    state.data = msg.position.back();
+};
 
 int setValeurPoint(trajectory_msgs::JointTrajectory* trajectoire,int pos_tab, int val){
     trajectoire->points[0].positions[pos_tab] = val;
     return 0;
 }
 
-
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "grasp");
-    ros::NodeHandle nh("~");
-    ros::Subscriber sub;
-    ros::Publisher pub;
-
-    ros::Rate loop_rate(10);
-
-    pub = nh.advertise<trajectory_msgs::JointTrajectory>("/soft_hand/soft_hand/joint_trajectory_controller/command", 1);
-
-    double g;
-
-    nh.getParam("grasp_value", g);    /// 0 - OPEN THE HAND
-                                      /// 1 - CLOSE THE HAND
+void grasp(double i){
 
     trajectory_msgs::JointTrajectory traj;
     trajectory_msgs::JointTrajectoryPoint points_n;
@@ -58,24 +60,45 @@ int main(int argc, char** argv) {
 
     traj.joint_names[0] ="soft_hand_synergy_joint";
 
-    while(ros::ok()) {
+    traj.header.stamp = ros::Time::now();
 
-        traj.header.stamp = ros::Time::now();
-
-        for(int j=0; j<1; j++) {
-            setValeurPoint(&traj,j,g);
-        }
-
-        traj.points[0].time_from_start = ros::Duration(1);
-
-        pub.publish(traj);
-        ros::spinOnce();
-
-        loop_rate.sleep();
+    for(int j=0; j<1; j++)
+    {
+            setValeurPoint(&traj,j,i);
     }
 
+    traj.points[0].time_from_start = ros::Duration(1);
+
+    pub.publish(traj);
+
+    ros::spinOnce();
+
+};
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "grasp");
+    ros::NodeHandle nh("~");
+    ros::Subscriber sub;
+
+    ros::Rate loop_rate(10);
+
+
+    sub = nh.subscribe("/soft_hand/joint_states", 1, jointStates);
+    pub = nh.advertise<trajectory_msgs::JointTrajectory>("/soft_hand/soft_hand/joint_trajectory_controller/command", 1);
+
+    double g;
+
+    nh.getParam("grasp_value", g);    /// 0 - OPEN THE HAND    /// 1 - CLOSE THE HAND
+
+
+    while(ros::ok()) {
+        grasp(g);
+
+        //if (state.data >= 0.5 ){
+           // ROS_INFO("GRASP IT");
+           // ros::ServiceServer service = nh.advertiseService("grasp_action", ack);
+            loop_rate.sleep();
+        //}
+    }
     return 0;
 }
-
-
-
